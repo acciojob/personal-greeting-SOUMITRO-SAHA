f7a8c7667971b5bdfbafe32708908c1bd28b8ded
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-07-19 11:51:59 +0530

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import InputBox from "./InputBox.Component";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<InputBox />
+		</div>
+	);
+};
 
-export default App
+export default App;
